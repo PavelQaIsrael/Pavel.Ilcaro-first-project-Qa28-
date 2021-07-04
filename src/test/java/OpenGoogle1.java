19d4c1e10245b3c4ca97d7c68931991463a65381
PavelQaIsrael <pavelqa.israel@gmail.com>
2021-07-04 12:55:01 +0300

    add Open Google1 class

---
+++ src/test/java/OpenGoogle1.java
@@ -0,0 +1,45 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+
+
+    public class OpenGoogle1 {
+        WebDriver wd;
+
+
+        @BeforeMethod
+        public void setUp(){
+        wd = new ChromeDriver();
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        wd.get("https://www.google.com/");
+
+
+        }
+
+        @Test
+        public void searchWebDriver(){
+
+            wd.findElement(By.name("q")).click();
+            wd.findElement(By.name("q")).clear();
+            wd.findElement(By.name("q")).sendKeys("WebDriver"+ Keys.ENTER);
+
+        }
+        @AfterMethod
+        public void tearDown() {
+            wd.quit();
+
+        }
+
+    }
+
+
+
